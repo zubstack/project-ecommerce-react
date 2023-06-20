d990df35fada770287abbe48e28939fa193cd52f
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-06-20 14:58:09 -0500

    Layout [product-detail] component

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -16,6 +16,25 @@ function ProductDetail() {
           <FaTimes onClick={() => closeProductDetails()} />
         </button>
       </div>
+      <div className="bg-white cursor-pointer w-full h-4/5 p-3">
+        <figure className="relative mb-2 w-full h-2/5">
+          <img
+            className="w-full h-full object-cover rounded-lg"
+            src=""
+            alt="headphone"
+          />
+        </figure>
+        <p className="flex justify-between">
+          <span className="card-span text-sm font-light">`data.title`</span>
+          <span className="card-span text-lg font-medium">`data.price`</span>
+        </p>
+        <p className="text-start">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero,
+          incidunt. Animi molestiae possimus cupiditate nobis distinctio soluta
+          temporibus tempore laboriosam ea placeat vel ipsa, doloribus saepe
+          labore ipsam delectus necessitatibus.
+        </p>
+      </div>
     </aside>
   );
 }
