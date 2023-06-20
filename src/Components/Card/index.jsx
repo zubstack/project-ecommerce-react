0c0684153d9c2eb43b1eb348912f55cc9427a901
Alejandro <mt.calderon.alejandro@gmail.com>
2023-06-19 20:29:58 -0500

    Create [useFetch] custom hook & render [cards] in [home] functionality

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,15 +1,15 @@
 import { FaPlus } from "react-icons/fa";
 
-function Card() {
+function Card({ data }) {
   return (
     <div className="bg-white cursor-pointer w-56 h-60">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left- bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
-          Electronics
+          {data.category.name}
         </span>
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://images.pexels.com/photos/3945667/pexels-photo-3945667.jpeg?auto=compress&cs=tinysrgb&w=300"
+          src={data.images[0]}
           alt="headphone"
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
@@ -17,8 +17,8 @@ function Card() {
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="card-span text-sm font-light">Headphones</span>
-        <span className="card-span text-lg font-medium">$300</span>
+        <span className="card-span text-sm font-light">{data.title}</span>
+        <span className="card-span text-lg font-medium">${data.price}</span>
       </p>
     </div>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,20 @@
+import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
+import useFetch from "../../Hooks/useFetch";
+import { urlApi } from "../../Data";
 
 function Home() {
+  const items = useFetch(`${urlApi}/products`);
+
   return (
-    <h1>
-      <Card />
-    </h1>
+    <Layout>
+      <h1 className="p-10">Home</h1>
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
+    </Layout>
   );
 }
 
