461fa5cb2446a29800041da381648b81c7185bd2
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-06-29 18:39:06 -0500

    Layout [sign-in]

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+
 function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+      <div className="flex flex-col w-80">
+        <label className="py-2" htmlFor="user-name">
+          User:{" "}
+        </label>
+        <input className="p-1" type="text" name="user-nam" />
+        <label className="py-2" htmlFor="user-password">
+          Password:{" "}
+        </label>
+        <input className="p-1" type="password" name="user-password" />
+        <Link to="/">
+          {" "}
+          <button className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2">
+            Log In
+          </button>
+        </Link>
+        <div className="text-center ">
+          <a
+            className="font-light text-xs underline underline-offset-4 "
+            href="/"
+          >
+            Forgot my password
+          </a>
+        </div>
+        <button className="border border-black disabled:text-black/40 disabled:border-black/40 rounded-lg mt-6 py-3">
+          {" "}
+          Sing Up
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default SignIn;
