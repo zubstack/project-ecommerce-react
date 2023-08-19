6e0cb4a7261ff33e9a5744d74b0ec89582c7f2f1
Ariel <mt.calderon.alejandro@gmail.com>
2023-08-19 06:18:31 -0500

    Rework

---
+++ src/Components/ShoppingCard/index.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { FaMinusCircle } from "react-icons/fa";
+import { ShoppingCartContext } from "../../Context";
+
+function ShoppingCard({ data, hasCloseButton }) {
+  const { removeFromShoppingCart } = useContext(ShoppingCartContext);
+
+  return (
+    <div className="flex justify-between items-center mb-3">
+      <div className="flex items-center gap-2">
+        <figure className="w-20 h-20">
+          <img
+            className="w-full h-full rounded-lg object-cover"
+            src={data.images[0]}
+            alt=""
+          />
+        </figure>
+        <p className="text-sm font-light text-center">{data.title}</p>
+      </div>
+      <div className="flex items-center gap-3">
+        <p className="text-lg font-medium tx-price">$ {data.price}</p>
+        {hasCloseButton ? (
+          <FaMinusCircle
+            className="h-3 w-3 text-black cursor-pointer mark-gray "
+            onClick={() => removeFromShoppingCart(data)}
+          ></FaMinusCircle>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ShoppingCard;
---
+++ src/Pages/OrderView/index.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import ShoppingCard from "../../Components/ShoppingCard";
+import { Link } from "react-router-dom";
+import { FaChevronLeft } from "react-icons/fa";
+
+function OrderView() {
+  const { orders } = useContext(ShoppingCartContext);
+  const currentPath = window.location.pathname;
+  let index = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+  if (index === "last") index = orders?.length - 1;
+  // console.log(index);
+
+  return (
+    <>
+      <div className="flex items-center justify-center relative w-80 mb-6">
+        <Link to="/my-orders" className="absolute left-0">
+          <FaChevronLeft className="h-4 w-4 text-black cursor-pointer" />
+        </Link>
+        <h1>Factura</h1>
+      </div>
+      <div>
+        {orders?.[index]?.products.map((product) => (
+          <ShoppingCard key={product.key} data={product} keyId={product.key} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default OrderView;
