a4752f2fba9b62571ef6f2d63bbffe1d0f46065b
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-18 13:24:55 -0500

    [cleaning code]

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,104 +2,43 @@ import Card from "../../components/Card";
 import ProductDetail from "../../components/ProductDetail";
 import ShoppingAside from "../../components/ShoppingAside";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import PromotionCard from "../../components/PromotionCard";
 import productsInPromotion from "../../utils/promotions";
 import { useProductContext } from "../../context/ProductContext";
 
 function Home() {
-  // const categoriesList = [
-  //   "clothes",
-  //   "furniture",
-  //   "electronics",
-  //   "toys",
-  //   "others",
-  // ];
-
-  // const navigate = useNavigate();
-
-  // //Read current category from path:
-  // const currentPath = window.location.pathname;
-  // let currentPathIndex = currentPath.substring(
-  //   currentPath.lastIndexOf("/") + 1
-  // );
-
-  // // Verify validation of the current catery:
-
-  // useEffect(() => {
-  //   categoriesList.includes(currentPathIndex) || currentPathIndex == "" ? (
-  //     <Home />
-  //   ) : (
-  //     navigate("/category/not-found")
-  //   );
-  // }, []);
-
   //Products: ==============================================================
-
   const { products } = useProductContext();
-  console.log("products", products);
 
-  // const [products, setProducts] = useState([]);
-  // useEffect(() => {
-  //   // productServices.getAll().then((data) => setProducts(data));
-  //   axios
-  //     .get(endpoints.getAll)
-  //     .then((data) => {
-  //       setProducts(data.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
-
-  //Products by category: [categoryProducts] =============================
-  //Choose the appropiate products:
-  // const categoryProducts = products?.filter((item) =>
-  //   item.category.name.toLowerCase().includes(currentPathIndex)
-  // );
-
-  // //Products by user filter: [filteredProducts] =============================
+  //Filter products: ===================================================
   const [userInput, setUserInput] = useState("");
   const [filteredProducts, setFilteredProducts] = useState("");
 
-  // //Selection:
   const filterItems = (userInput) => {
     return products?.filter((product) =>
       product.item.name.toLowerCase().includes(userInput.toLowerCase())
     );
   };
-  //Setting (if user inputs):
   useEffect(() => {
     if (userInput) setFilteredProducts(filterItems(userInput));
   }, [userInput]);
 
-  console.log("userInput", userInput);
-
-  //Details: ==========================================================
-
+  //Details : ======================================================
   const [detailsOpen, setDetailsOpen] = useState(false);
   const closeProductDetails = () => setDetailsOpen(false);
   const openProductDetails = () => setDetailsOpen(true);
 
   //Shopping Cart : ======================================================
-
   const [shoppingOpen, setShoppingOpen] = useState(false);
   const openShoppingAside = () => setShoppingOpen(true);
   const closeShoppingAside = () => setShoppingOpen(false);
 
-  //Render functions: ==========================================================
-
+  //Render function: ====================================================
   const renderView = () => {
     const productsToRender = userInput.length > 0 ? filteredProducts : products;
 
-    //   //PENDING: Path category - All category is valid
-    //   //PENDING: If a category doesnt have items, should be display "New items here soon..."
-    //   //PENDING: Semanal offerts
-    //   //PENDING: Loading state
-    //   //PENDING: Scroll bar
     if (productsToRender?.length > 0) {
       return productsToRender.map((item) => (
-        // return products.map((item) => (
         <Card
           key={item.id}
           openProductDetails={openProductDetails}
