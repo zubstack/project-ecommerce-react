bdd3b5a52548d8158bc0b91c8f030a00286127e1
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-19 09:50:48 -0500

    [update]: base url

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -1,5 +1,5 @@
 // const baseUrl = "https://api.escuelajs.co/api/v1/products";
-const baseUrl = "http://localhost:4000";
+const baseUrl = import.meta.env.VITE_BASE_URL;
 
 const endpoints = {
   getAll: `${baseUrl}/api/products`,
