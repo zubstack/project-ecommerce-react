e6dacc1d8ad19886b826652d7fa65ac273ceb1d5
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2024-02-19 19:42:54 -0500

    layout navtop

--- src/ui/NavbarDesktop/index.jsx
+++ src/ui/NavbarDesktop/index.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import './style.css';
 
 function NavbarDesktop() {
-  return <div className='bg-amber-500 px-10'>NavbarDesktop</div>;
+  return (
+    <div className='bg-orange-400 px-10 '>
+      <ul id='desktop__navbar'>
+        <li>Homepage</li>
+        <li>Basketball Shoes</li>
+        <li>Offers</li>
+      </ul>
+    </div>
+  );
 }
 
 export default NavbarDesktop;
